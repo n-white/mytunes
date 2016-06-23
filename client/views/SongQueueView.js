0b965e956c5501f5a72e5310dcad2afb12b8fb38
n-white <neil.r.whitehead@gmail.com>
2016-06-22 20:05:19 -0700

    "songQueueView 1 test passed. Render still not passing"

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,9 +2,22 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    // debugger;
+    this.collection.on('add', this.render, this);
+    this.collection.on('remove', this.render, this);
+
   },
 
   render: function () {
+    var html = [
+      '<div>',
+        '<span>',
+          this.collection,
+        '</span>',
+      '</div>',
+      '<br/>'
+    ].join('');
+    $('body').append(html);
   }
 
 });
